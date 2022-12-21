a979c96bfa35e73aa6384e1d7620c8bcc0239b4a
Jhimy2000 <u201818218@upc.edu.pe>
2022-12-20 21:25:04 -0500

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,13 @@
+//cuando las varibles y las funciones se procesan antes de que se ejecuten cualquier tipo de codigo
+
+console.log(miNombre);
+
+var miNombre="Jhimy";
+
+
+hey();
+function hey(){
+    console.log("Hola" + miNombre);
+}
+
+var miNombre="Lucas";
