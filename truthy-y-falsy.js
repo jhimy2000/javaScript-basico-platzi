35ce0aaca6aa97813aeba497c2976e7a68a6d722
Jhimy2000 <u201818218@upc.edu.pe>
2022-12-21 02:47:32 -0500

    truthy-y-falsy

---
+++ truthy-y-falsy.js
@@ -0,0 +1,7 @@
+//Ejemplos en los que Boolean devuelve Falso:
+Boolean(0); //false
+Boolean(null); //false
+Boolean(NaN); //false
+Boolean(undefined); //false
+Boolean(false); //false
+Boolean(""); //false
