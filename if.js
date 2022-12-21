eb51c565904e48c071f180e63ebe1cfd5ea6cfcf
Jhimy2000 <u201818218@upc.edu.pe>
2022-12-21 03:44:40 -0500

    switch

---
+++ if.js
@@ -0,0 +1,36 @@
+var j = 0;
+
+if (j == 0) {
+  console.log("Hola");
+} else if (j == 1) {
+  console.log("Es falsa");
+} else {
+  console.log("Me rindo");
+}
+
+condition ? true : false;
+
+vmoroe ? true: false;
+
+
+var op1 = "Piedra";
+var op2 = "Papel";
+var op3 = "Tijera";
+
+var resultado = function(user, cpu){
+    if(user != cpu){
+        if(user === op1 && cpu === op3){
+            console.log("el usuario GANO con "+ op1)
+        }else if(user === op2 && cpu === op1){
+            console.log( "el usuario GANO con " + op2)
+        }else if(user === op3 && cpu === op2){
+            console.log("el usuario GANO con " + op3)
+        }else{
+            console.log("La CPU Gano!!")
+        }
+    }else if(user === cpu){
+        console.log("Empate")
+    }
+};
+
+resultado(op1,op3) //el usuario GANO con Piedra```
