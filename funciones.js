49e52a7fcfa4d2002836785dfbbd2cb75671799e
Jhimy2000 <u201818218@upc.edu.pe>
2022-12-20 21:08:55 -0500

    scope

---
+++ funciones.js
@@ -0,0 +1,17 @@
+//Declarativas
+
+function miFuncion(){
+    return 3;
+}
+
+miFuncion();
+
+//Expresion 
+
+var miFuncionn=function(a,b){
+
+
+    return a+b;
+}
+
+miFuncionn();
---
+++ scope.js
@@ -0,0 +1,6 @@
+var miNombre="Jhimy";
+
+function nombre(){
+    var miApellido="Diestra Gaspar";
+    console.log(miNombre+" "+miApellido);
+}
